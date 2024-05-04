5c915850344d7e7bf1b793166e792465376709c5
kotik322322 <kotan23111995@gmail.com>
2024-05-05 02:13:09 +0300

    fixed bg video

--- app/components/HeroPage.tsx
+++ app/components/HeroPage.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Video from 'next-video';
+import bgVideo from "../../videos/bgVideo.mp4"
 
 const HeroPage = () => {
-  return <video src="/bgVideo.mp4" loop autoPlay muted className="" />;
+  return (
+    <Video
+      src={bgVideo}
+      autoPlay
+      muted
+      loop
+      playsInline
+      controls={false}
+      className=""></Video>
+  );
 };
 
 export default HeroPage;
