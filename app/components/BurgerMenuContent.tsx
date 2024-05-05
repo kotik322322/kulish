c4f747fba064a37255a16dbfab2176a7ee0e8f8a
kotik322322 <kotan23111995@gmail.com>
2024-05-05 21:46:40 +0300

    fexed Links text size

--- app/components/BurgerMenuContent.tsx
+++ app/components/BurgerMenuContent.tsx
@@ -17,7 +17,7 @@ const BurgerMenuContent = ({ isOpen }: BurgerMenuContentProps) => {
           exit={{ x: '-100%', transition: { duration: 0.5 } }}
           className="fixed top-0 bottom-0 left-0 right-0 bg-accent/50 w-full h-full z-40 text-white">
           <ul className="w-full h-full flex flex-col justify-center items-center gap-y-8 font-semibold text-xl">
-            <CustomLink title="About" href="/" />
+            <CustomLink title="About" href="/about" />
             <CustomLink title="Content" href="/" />
             <CustomLink title="Contact" href="/" />
             <CustomLink title="Gallery" href="/" />
--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import logo from "../../public/logo.svg"
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Logo = () => {
   return (
-    <div className='absolute w-[100px] h-[32px] left-1/2 -translate-x-1/2 top-4 z-50'>
+    <Link href="/" className='inline-block absolute w-[100px] h-[32px] left-1/2 -translate-x-1/2 top-4 z-50'>
       <Image src={logo} alt='Logo'/>
-    </div>
+    </Link>
   )
 }
 
