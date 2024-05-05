6d966013538ba47e902ee490d86c867b670ac060
kotik322322 <kotan23111995@gmail.com>
2024-05-05 21:52:05 +0300

    fexed Links text size

--- app/components/CustomLink.tsx
+++ app/components/CustomLink.tsx
@@ -15,7 +15,7 @@ const CustomLink = ({ href, title }: CustomLinkProps) => {
         {title}
         <span
           className={`h-[1px] inline-block w-0  bg-white absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
-            pathName === href ? 'w-full' : 'w-0'
+            pathName === href ? 'w-full' : 'w-0' 
           }`}>
           &nbsp;
         </span>
