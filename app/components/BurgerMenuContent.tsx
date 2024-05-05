02e3aeeb380af4354e069241c11cf403a3a0fed8
kotik322322 <kotan23111995@gmail.com>
2024-05-05 21:42:52 +0300

    fexed Links text size

--- app/components/BurgerMenuContent.tsx
+++ app/components/BurgerMenuContent.tsx
@@ -15,8 +15,8 @@ const BurgerMenuContent = ({ isOpen }: BurgerMenuContentProps) => {
           initial={{ x: '-100%' }}
           animate={{ x: 0, transition: { duration: 0.5 } }}
           exit={{ x: '-100%', transition: { duration: 0.5 } }}
-          className="fixed top-0 bottom-0 left-0 right-0 bg-accent/80 w-full h-full z-40 text-white">
-          <ul className="w-full h-full flex flex-col justify-center items-center gap-y-10">
+          className="fixed top-0 bottom-0 left-0 right-0 bg-accent/50 w-full h-full z-40 text-white">
+          <ul className="w-full h-full flex flex-col justify-center items-center gap-y-8 font-semibold text-xl">
             <CustomLink title="About" href="/" />
             <CustomLink title="Content" href="/" />
             <CustomLink title="Contact" href="/" />
