c740362706ad2ee359a58d4b59ef0883086c7aaf
kotik322322 <kotan23111995@gmail.com>
2024-05-05 19:14:56 +0300

    fixed bugs with video

--- app/components/HeroPage.tsx
+++ app/components/HeroPage.tsx
@@ -7,7 +7,7 @@ import BackgroundVideo from 'next-video/background-video';
 
 const HeroPage = () => {
   return (
-    <video src="/bgVideo.mp4" autoPlay muted className='w-full h-full object-cover overflow-hidden'/>
+    <video src="/bgVideo.mp4" autoPlay muted playsInline className='w-full h-full object-cover overflow-hidden'/>
   );
 };
 
